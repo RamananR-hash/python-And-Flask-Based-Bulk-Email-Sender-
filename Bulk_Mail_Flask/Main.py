745ffb6980486a46bb54f56fb72733af376a33ca
Ramanan <38250400+RamananR-hash@users.noreply.github.com>
2021-11-21 21:07:59 +0530

    Add files via upload

---
+++ Bulk_Mail_Flask/Main.py
@@ -0,0 +1,60 @@
+import smtplib
+from email.message import EmailMessage
+import csv
+#Global variables
+credit =500
+new_credit =0
+def maincall(UserName,Password,Subject,Body,filenamein,attach1,attach2):
+    #print(user,pass67,sub67,body67,filenamein)
+    filenamein=str(filenamein)
+    attach1=str(attach1)
+    attach2=str(attach2)    
+    with open('D:/Bulk_Mail_Flask/DataBase/'+filenamein, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            length=len(row)
+            for i in range (0,length,1):
+                To_Email=(row[i])
+                try:
+                    #print("Try Loop")
+                    msg      = EmailMessage()
+                    username = str(UserName)
+                    password = str(Password)
+                    to       = To_Email
+                    subject  = str(Subject)
+                    body     = str(Body)
+                    msg['subject'] = subject
+                    msg['from'] = username
+                    msg['to'] = to
+                    msg.set_content(body)
+                    attachments=[]
+                    filename1 = str("D:/Bulk_Mail_Flask/Attachments/"+attach1)
+                    attachments.append(filename1)
+                    filename2 = str('D:/Bulk_Mail_Flask/Attachments/'+attach2)
+                    attachments.append(filename2)
+                    #print(attachments)
+                    for i in range(0,len(attachments),1):
+                        with open(attachments[i], 'rb') as f:
+                            file_data = f.read()
+                            print(attachments[i])
+                        msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=f.name)
+                    if username=="" or password=="" or to=="" or subject=="" or body=="":
+                        print("All fields required")
+                    else:
+                        server   = smtplib.SMTP('smtp.gmail.com',587)
+                        server.starttls()
+                        server.login(username, password)
+                        server.send_message(msg)
+                        print("Email Delivried")
+                        new_credit=credits-1
+                        credits=new_credit
+                        result="Successfully"
+                        print(new_credit)
+                except:
+                    print("Error Sending Mail")
+                    result="Not Successfully"
+                    new_credit=credit
+                    print(new_credit) 
+        return result,new_credit   
+#status,credits_remain=maincall("info.weedirect@gmail.com","weedirect@123","hello","helo","Email.csv","imagesd.png","imagesd.png",credit)
+#print(status,credits_remain)
---
+++ Bulk_Mail_Flask/app.py
@@ -0,0 +1,51 @@
+from logging import root
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
+from Main import  maincall
+import os 
+UPLOAD_FOLDER = 'D:/Bulk_Mail_Flask/DataBase'
+UPLOAD_FOLDER1 = 'D:/Bulk_Mail_Flask/Attachments'
+
+
+app=Flask(__name__, template_folder='templates')
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER1'] = UPLOAD_FOLDER1
+
+
+@app.route('/')
+@app.route('/home')
+
+def home():
+    return render_template('register.html')
+
+@app.route('/confirm',methods=['POST','GET'])
+def register():
+    if request.method =='POST':
+        UserName = request.form.get('Username')
+        Password = request.form.get('Password')
+        Subject = request.form.get('subject')
+        Body = request.form.get('body')
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+        if request.files['attach1'].filename == '':
+            print("No Files")
+            attachment1_filename="imagesd.png"
+        else:
+            attachment1 = request.files['attach1']
+            attachment1.save(os.path.join(app.config['UPLOAD_FOLDER1'],attachment1.filename))
+            attachment1_filename = attachment1.filename
+        if request.files['attach2'].filename == '':
+            print("No Files")
+            attachment2_filename="imagesd.png"
+        else:
+            attachment2 = request.files['attach2']
+            attachment2.save(os.path.join(app.config['UPLOAD_FOLDER1'],attachment2.filename))
+            attachment2_filename=attachment2.filename
+        msg_status,credits_bal=maincall(UserName,Password,Subject,Body,f.filename,attachment1_filename,attachment2_filename)
+        print(msg_status,credits_bal)
+
+        return render_template("output.html",name_out=UserName,password_out=Password,subject_out=Subject,body_out=Body,Email_list=f.filename,Status_mail=msg_status,crd=credits_bal)             
+if __name__ =="__main__":
+    app.run(debug=True)
